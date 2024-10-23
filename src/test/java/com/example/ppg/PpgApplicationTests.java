87c75c907e4c41bab08aec14de3fb9dc267a5b70
angie2512 <a20191792@pucp.edu.pe>
2024-10-22 20:50:16 -0500

    primer commit

---
+++ src/test/java/com/example/ppg/PpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
